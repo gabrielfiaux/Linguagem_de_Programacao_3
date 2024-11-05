35634a48206c419f4d1f6ba790f99255679261fa
gabrielfiaux <gfiauxreis@gmail.com>
2024-11-05 19:09:01 -0300

    Commit
    Esqueci oque fiz

---
+++ 3° Bimestre/JPAHibernet/src/main/java/br/edu/ifpr/bean/Categoria.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifpr.bean;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+/**
+ *
+ * @author gfiau
+ */
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long  id;
+    
+    @Column(nullable = false)
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
--- 3° Bimestre/JPAHibernet/src/main/java/br/edu/ifpr/bean/Teste.java
+++ 3° Bimestre/JPAHibernet/src/main/java/br/edu/ifpr/bean/Teste.java
@@ -4,7 +4,25 @@
  */
 package br.edu.ifpr.bean;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 import org.hibernate.jpa.HibernatePersistenceProvider;
 public class Teste {
+    public static void main(String[] args) {
+        
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("mysql_db");
+        EntityManager em = emf.createEntityManager();
+        
+        Categoria categoria = new Categoria();
+        categoria.setNome("Moveis");
+        em.getTransaction().begin();
+        em.persist(categoria);
+        em.getTransaction().commit();
+        em.close();
+        emf.close();
+                
+    }
+    
     
 }
